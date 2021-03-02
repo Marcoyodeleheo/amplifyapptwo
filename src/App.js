45fa396f31ecf05b13d6e1e4f78b779f712102e5
Marcoyodeleheo <marco-gonzalez@bluewin.ch>
2021-03-02 21:11:29 +0100

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>TELLBOT</code>
+          <code>TELLBOT und JINN-BOT</code>
         </p>
         <a
           className="App-link"
